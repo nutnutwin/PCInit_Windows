80c08bc2478f16ca701a2a2fae5a680053867e15
nutnutwin <nutnutwin@gmail.com>
2025-01-29 08:52:48 +0900

    2025/01/29_ 8:52:48.21_[HOME_3600]_nutnu

--- 03_Communication/[[Communication]].bat
+++
@@ -1,7 +0,0 @@
-﻿@REM Must have Space before brackets
-@REM Must not have ""(Quotation)
-IF EXIST C:\App_Local\doublecmd\doublecmd.exe (
-	C:\App_Local\doublecmd\doublecmd.exe -P L -T %0
-) ELSE (
-	start .
-)
